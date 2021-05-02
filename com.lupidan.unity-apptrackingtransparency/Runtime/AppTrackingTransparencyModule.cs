dadebc6946720176f702dd6e9022ee38d646c336
Daniel Lupiañez Casares <lupidan@gmail.com>
2021-05-02 16:28:18 +0200

    Adds Module class to return a IAppTransparencyManager or throw an exception depending on the platform

---
+++ com.lupidan.unity-apptrackingtransparency/Runtime/AppTrackingTransparencyModule.cs
@@ -0,0 +1,36 @@
+#if UNITY_IOS || UNITY_TVOS || UNITY_STANDALONE_OSX
+#define PLATFORM_SUPPORTS_APPTRACKINGTRANSPARENCY
+#endif
+
+using AppTrackingTransparency.Common;
+
+namespace AppTrackingTransparency
+{
+    public static class AppTrackingTransparencyModule
+    {
+        public static bool IsSupported
+        {
+            get
+            {
+                #if PLATFORM_SUPPORTS_APPTRACKINGTRANSPARENCY
+                    return true;
+                #else
+                    return false;
+                #endif
+            }
+        }
+
+        public static IAppTrackingTransparencyManager CreateManager()
+        {
+            #if PLATFORM_SUPPORTS_APPTRACKINGTRANSPARENCY
+                #if UNITY_EDITOR
+                    return new Editor.EditorAppTrackingTransparencyManager();
+                #else
+                    return new Native.NativeAppTrackingTransparencyManager();
+                #endif
+            #else
+                throw new System.Exception("Unsupported platform");
+            #endif
+        }
+    }
+}
