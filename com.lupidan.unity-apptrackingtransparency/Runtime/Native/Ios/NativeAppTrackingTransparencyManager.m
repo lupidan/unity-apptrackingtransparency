23f58d3dd85bb9bc5b52519e890b97fa2cd6bd55
Daniel Lupiañez Casares <lupidan@gmail.com>
2021-05-02 16:45:16 +0200

    Adds first version for native code wrapper

---
+++ com.lupidan.unity-apptrackingtransparency/Runtime/Native/Ios/NativeAppTrackingTransparencyManager.m
@@ -0,0 +1,89 @@
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 140000  || __TV_OS_VERSION_MAX_ALLOWED >= 140000 || __MAC_OS_X_VERSION_MAX_ALLOWED >= 110000
+#define APP_TRACKING_TRANSPARENCY_AVAILABLE true
+#import <AppTrackingTransparency/AppTrackingTransparency.h>
+#endif
+
+#pragma mark - NativeAppTrackingTransparencyManager
+
+typedef void (*RequestTrackingAuthorizationCallbackDelegate)(uint requestId,  uint authorizationStatus);
+
+@interface NativeAppTrackingTransparencyManager : NSObject
+@property (nonatomic, assign) RequestTrackingAuthorizationCallbackDelegate requestTrackingAuthorizationCallback;
+@property (nonatomic, weak) NSOperationQueue *callingOperationQueue;
+@end
+
+@implementation NativeAppTrackingTransparencyManager
+
+static const uint AppTrackingTransparencyManagerAuthorizationStatusAuthorized = 3;
+
++ (instancetype) sharedManager {
+    static NativeAppTrackingTransparencyManager *_defaultManager = nil;
+    static dispatch_once_t defaultManagerInitialization;
+    
+    dispatch_once(&defaultManagerInitialization, ^{
+        _defaultManager = [[NativeAppTrackingTransparencyManager alloc] init];
+    });
+    
+    return _defaultManager;
+}
+
+- (uint) rawTrackingAuthorizationStatus {
+#ifdef APP_TRACKING_TRANSPARENCY_AVAILABLE
+    if (@available(iOS 14.0, tvOS 14.0, macOS 11.0, *)) {
+        return (uint)[ATTrackingManager trackingAuthorizationStatus];
+    } else {
+        return AppTrackingTransparencyManagerAuthorizationStatusAuthorized;
+    }
+#else
+    return AppTrackingTransparencyManagerAuthorizationStatusAuthorized;
+#endif
+}
+
+- (void) initiateTrackingAuthorizationRequestWithRequestId:(uint)requestId {
+#ifdef APP_TRACKING_TRANSPARENCY_AVAILABLE
+    if (@available(iOS 14.0, tvOS 14.0, macOS 11.0, *)) {
+        [ATTrackingManager requestTrackingAuthorizationWithCompletionHandler:^(ATTrackingManagerAuthorizationStatus status) {
+            [self handleTrackingAuthorizationRequestResult:(uint)status forRequestId:requestId];
+        }];
+    } else {
+        [self handleTrackingAuthorizationRequestResult:AppTrackingTransparencyManagerAuthorizationStatusAuthorized forRequestId:requestId];
+    }
+#else
+    [self handleTrackingAuthorizationRequestResult:AppTrackingTransparencyManagerAuthorizationStatusAuthorized forRequestId:requestId];
+#endif
+    
+}
+
+- (void) handleTrackingAuthorizationRequestResult:(uint)rawAuthorizationRequest forRequestId:(uint)requestId {
+    if ([self callingOperationQueue]) {
+        [[self callingOperationQueue] addOperationWithBlock:^{
+            if ([self requestTrackingAuthorizationCallback] != NULL) {
+                [self requestTrackingAuthorizationCallback](requestId, requestId);
+            }
+        }];
+    } else {
+        if ([self requestTrackingAuthorizationCallback] != NULL) {
+            [self requestTrackingAuthorizationCallback](requestId, requestId);
+        }
+    }
+}
+
+@end
+
+#pragma mark - Native C Calls
+
+uint AppTrackingTransparencyManager_GetTrackingAuthorizationStatus()
+{
+    return [[NativeAppTrackingTransparencyManager sharedManager] rawTrackingAuthorizationStatus];
+}
+
+void AppTrackingTransparencyManager_SetRequestTrackingAuthorizationCallbackHandler(RequestTrackingAuthorizationCallbackDelegate callbackHandler)
+{
+    [[NativeAppTrackingTransparencyManager sharedManager] setRequestTrackingAuthorizationCallback:callbackHandler];
+    [[NativeAppTrackingTransparencyManager sharedManager] setCallingOperationQueue:[NSOperationQueue currentQueue]];
+}
+
+void AppTrackingTransparencyManager_RequestTrackingAuthorizationCallback(uint requestId)
+{
+    [[NativeAppTrackingTransparencyManager sharedManager] initiateTrackingAuthorizationRequestWithRequestId:requestId];
+}
